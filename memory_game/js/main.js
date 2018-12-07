dabd96a7447032852b467c94838b0a2fddb93812
NATHAN LAMONT SMITH <andinto@gmail.com>
2018-12-06 19:53:32 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,23 @@
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+var cardsInPlay = []; 
+
+var cardOne = cards[0];
+
+var cardTwo = cards[2]; 
+
+
+cardsInPlay.push(cardOne); 
+console.log("User flipped " + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo); 
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
