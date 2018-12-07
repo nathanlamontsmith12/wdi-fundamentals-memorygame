7c6cb0f6e6b2b58d539ac24b4db811b055b26c36
NATHAN LAMONT SMITH <andinto@gmail.com>
2018-12-06 18:10:03 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
